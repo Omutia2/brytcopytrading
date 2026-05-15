926f890561bbe701519b50150877b3c5b6f505a6
TheUgTrader <mauriceajheiz@gmail.com>
2026-05-15 16:36:20 +0300

    hggfff

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     @include('dashboard.partials.header')
     @include('dashboard.partials.summary-cards')
-    @include('dashboard.partials.charts')
+    {{-- @include('dashboard.partials.charts') --}}
     @include('dashboard.partials.active-trades')
 @endsection
